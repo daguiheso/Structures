761cbc5ec02fc0a9a5ea266ec87dee52b5e1bbf4
Guillermo Hernandez <frontend@reddesignsystems.com>
2015-06-02 15:26:45 -0500

    Update Structure Web Gulp&Elixir

--- Web Gulp-Elixir/gulpfile.js
+++ Web Gulp-Elixir/gulpfile.js
@@ -5,9 +5,8 @@ elixir(function(mix) {
     mix.less("style.less", "assets/css");
 
     mix.scripts([
-    	"jquery/jquery.min.js",
-    	"bootstrap/bootstrap.min.js",
-    	"app.js"
+    	"libs/jquery/jquery-2.1.4.min.js",
+    	"libs/bootstrap/bootstrap-3.3.4.min.js",
     ], "assets/js", "resources/assets/js");
 
 });
